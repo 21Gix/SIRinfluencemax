d8868f4f95834b93cbc5e6954d3d4c3f463975b9
Giacomo Casagranda <105413843+21Gix@users.noreply.github.com>
2022-12-06 17:27:44 +0100

    Cycle through file

--- main.py
+++ main.py
@@ -5,14 +5,22 @@ class Node:
 
 
 class Msg:
-    def __init__(self, id, src, dest, eta):
+    def __init__(self, id, src, dst, eta):
         self.id = id
         self.src = src
-        self.dest = dest
+        self.dst = dst
         self.eta = eta
 
 
 if __name__ == '__main__':
-    filename = input('Enter the filename for the dataset: ')
+    filename = input('Enter the filename for the dataset(must be .txt): ')
 
-    print('Opening file ', filename, ':')
+    # dataset import
+    print('Opening file :', filename)
+    f = open(filename, 'r')
+
+    # preparing messages list
+    msgList = []
+
+    for x in f:
+        print(x, end='')
